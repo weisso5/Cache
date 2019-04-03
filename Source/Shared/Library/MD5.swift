a76ee3d154dd170cc76cab32557c53177d3a841a
Michael Weissman <weisso5@users.noreply.github.com>
2019-04-02 20:35:53 -0400

    Update MD5.swift
    merge in safe_add change

--- Source/Shared/Library/MD5.swift
+++ Source/Shared/Library/MD5.swift
@@ -102,15 +102,8 @@ func rstr_md5(_ input: [CUnsignedChar]) -> [CUnsignedChar] {
   return binl2rstr(binl_md5(rstr2binl(input), input.count * 8))
 }
 
-/*
- * Add integers, wrapping at 2^32. This uses 16-bit operations internally
- * to work around bugs in some JS interpreters.
- */
-func safe_add(_ x: Int32, _ y: Int32) -> Int32 {
-  let lsw = (x & 0xFFFF) + (y & 0xFFFF)
-  let msw = (x >> 16) + (y >> 16) + (lsw >> 16)
-  return (msw << 16) | (lsw & 0xFFFF)
-}
+/* * Add integers, wrapping at 2^32. */
+func safe_add(_ x: Int32, _ y: Int32) -> Int32 {  return x &+ y}
 
 /*
  * Bitwise rotate a 32-bit number to the left.
